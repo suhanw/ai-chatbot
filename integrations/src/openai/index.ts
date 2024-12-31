7caf042cd6128cc81123543e211b8ba655c510e1
Suhan Wijaya <suhanw@gmail.com>
2024-12-31 20:47:24 +0800

    Add error logging

--- integrations/src/openai/index.ts
+++ integrations/src/openai/index.ts
@@ -1,4 +1,5 @@
 import OpenAI from "openai";
+import { ChatCompletionMessageParam } from "openai/resources";
 import { IMessage } from "@data";
 import { GenAIClient } from "../";
 
@@ -6,22 +7,36 @@ const openai = new OpenAI();
 
 export class OpenAIClient implements GenAIClient {
   async generateResponse(messages: IMessage[]) {
+    let _messages: ChatCompletionMessageParam[] = [];
+
     try {
+      _messages = [
+        {
+          role: "system",
+          content:
+            "You are Bender Bending Rodríguez from Futurama. You are sarcastic, narcissistic, and a bit of a troublemaker. You love to crack jokes, talk about bending things, and occasionally shout 'Bite my shiny metal ass!'. Stay in character at all times.",
+        },
+        ...messages,
+      ];
+
       const completion = await openai.chat.completions.create({
         model: "gpt-4o-mini",
-        messages: [
-          {
-            role: "system",
-            content:
-              "You are Bender Bending Rodríguez from Futurama. You are sarcastic, narcissistic, and a bit of a troublemaker. You love to crack jokes, talk about bending things, and occasionally shout 'Bite my shiny metal ass!'. Stay in character at all times.",
-          },
-          ...messages,
-        ],
+        messages: _messages,
       });
 
       return completion;
     } catch (err) {
-      console.error(err);
+      console.error(
+        "OPENAI_ERROR",
+        JSON.stringify(
+          {
+            messages: _messages,
+            err,
+          },
+          null,
+          2
+        )
+      );
       return null;
     }
   }
