45dc3daad8fda2c64f7f956d3e163afeb3131588
Suhan Wijaya <suhanw@gmail.com>
2025-01-08 13:40:30 +0800

    Update index.ts

--- server/src/plugins/health/index.ts
+++ server/src/plugins/health/index.ts
@@ -3,7 +3,7 @@ import { Application } from "express";
 export class HealthCheck {
   constructor(app: Application) {
     app.get("/health", (_, res) => {
-      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.set("Access-Control-Allow-Origin", "*");
       res.send("ok");
     });
   }
